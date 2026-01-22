a16db6787094bdd2ffd08e7d44a57102068fc032
pixodesk <pxdeskcomp@gmail.com>
2026-01-22 14:04:02 +0000

    add Starlight documentation structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import yaml from "@modyfi/vite-plugin-yaml";
 import mdx from '@astrojs/mdx';
-
+import starlight from "@astrojs/starlight";
 
 export default defineConfig({
     markdown: {
@@ -24,7 +24,34 @@ export default defineConfig({
             allowedHosts: ['pixodesk.com'], // Domain
         }
     },
-    integrations: [react(), tailwind({
-        config: { applyBaseStyles: true },
-    }), mdx()],
+    integrations: [
+        react(), 
+        tailwind({
+            config: { applyBaseStyles: true },
+        }), 
+        starlight({
+            title: "Pixodesk Docs",
+            disable404Route: true,
+            // customCss: ['./src/styles/starlight-custom.css'], // Custom css
+            tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 3 },
+            sidebar: [
+                {
+                    label: 'Getting Started',
+                    items: [
+                        { label: 'Introduction', slug: 'docs' },
+                    ],
+                },
+                {
+                    label: 'Lottie for Web',
+                    autogenerate: { directory: 'docs/web' },
+                },
+                {
+                    label: 'Lottie for Mobile',
+                    autogenerate: { directory: 'docs/mobile' },
+                },
+            ],
+        }),
+        mdx(),
+        
+    ],
 });
