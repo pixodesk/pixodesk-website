17b4e6536dbe00477770438d5228883f945feb5f
pixodesk <pxdeskcomp@gmail.com>
2026-02-05 11:25:08 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,6 +34,64 @@ export default defineConfig({
         react(),
         tailwind({
             config: { applyBaseStyles: true },
+<<<<<<< Updated upstream
         })
+=======
+        }),
+        starlight({
+            title: "Pixodesk",
+            disable404Route: true,
+            pagefind: false,
+            locales: {
+                root: {
+                    label: 'English',
+                    lang: 'en',
+                },
+            },
+            defaultLocale: 'root',
+            expressiveCode: {
+                themes: ['github-light', 'github-dark'],  // Use light syntax theme, github-light, min-light, slack-ochin, solarized-light, vitesse-light
+
+                frames: {
+                    terminalTitlebarDotsOpacity: '0',      // Hide the dots
+                    terminalTitlebarBorderBottomColor: 'transparent',
+                    editorTabBarBorderBottomColor: 'transparent',
+                },
+
+                styleOverrides: {
+                    frames: {
+                        showCopyToClipboardButton: true,  // keep copy button
+                        frameBoxShadowCssValue: 'none',
+                    },
+                },
+
+            },
+            // customCss: ['./src/styles/starlight-custom.css'], // Custom css
+            customCss: ['./src/styles/starlight-custom-style.css'], 
+            tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 3 },
+            components: {
+                // Header: './src/components/starlight/Header.astro',
+                // Footer: './src/components/starlight/Footer.astro',
+            },
+            sidebar: [
+                {
+                    label: 'Getting Started',
+                    items: [
+                        { label: 'Introduction', slug: 'docs' },
+                    ],
+                },
+                {
+                    label: 'Lottie for Web',
+                    autogenerate: { directory: 'docs/web' },
+                },
+                {
+                    label: 'Lottie for Mobile',
+                    autogenerate: { directory: 'docs/mobile' },
+                },
+            ],
+        }),
+        mdx(),
+
+>>>>>>> Stashed changes
     ],
 });
