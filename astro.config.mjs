9eec62496f33d564274fb1cb7a95bcb309737f76
pixodesk <pxdeskcomp@gmail.com>
2026-02-09 19:23:21 +0000

    add docs introduction, lottie introduction and react-lottie text

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,6 @@ export default defineConfig({
         react(),
         tailwind({
             config: { applyBaseStyles: true },
-        })
         }),
         starlight({
             title: "Pixodesk",
